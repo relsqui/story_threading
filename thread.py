08cb42e15c62d8837991d7dcfadf70b947401d7a
Finn Ellis <relsqui@chiliahedron.com>
2016-08-02 14:15:45 -0700

    Use command line arg for chapter file.

--- thread.py
+++ thread.py
@@ -2,8 +2,15 @@
 
 import random
 import json
+import sys
 
-with open("chapter.json") as f:
+if len(sys.argv) == 2:
+    chapterfile = sys.argv[1]
+else:
+    print "Please pass exactly one argument: a chapter filename."
+    sys.exit(1)
+
+with open(chapterfile) as f:
     chapter = json.loads(f.read())
 
 random.shuffle(chapter["scenes"])
