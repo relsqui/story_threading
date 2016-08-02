398888f1e3cdf6e120e895d0f3e4fc3511e91f64
Finn Ellis <relsqui@chiliahedron.com>
2016-08-02 14:41:40 -0700

    Fix error in scene indexing.

--- thread.py
+++ thread.py
@@ -26,6 +26,6 @@ random.shuffle(chapter["scenes"])
 print chapter["introduction"]
 print
 for i in range(len(chapter["story"])):
-    print chapter["scenes"][0].format(**chapter["story"][i])
+    print chapter["scenes"][i].format(**chapter["story"][i])
     print
 print chapter["conclusion"]
