608d32c121a4938653052ff1748171ded8f8eb69
Finn Ellis <relsqui@chiliahedron.com>
2016-08-03 13:13:59 -0700

    Use string pronoun arguments (easier to call from outside).

--- character.py
+++ character.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
-THEY = ["they", "them", "their", "theirs", "themselves"]
-PKEY = {"they":0, "them":1, "their":2, "theirs":3, "themselves":4}
-SHE = ["she", "her", "her", "hers", "herself"]
-HE = ["he", "him", "his", "his", "himself"]
+pronouns = {}
+pronouns["PINDEX"] = {"they":0, "them":1, "their":2, "theirs":3, "themselves":4}
+pronouns["THEY"] = ["they", "them", "their", "theirs", "themselves"]
+pronouns["SHE"] = ["she", "her", "her", "hers", "herself"]
+pronouns["HE"] = ["he", "him", "his", "his", "himself"]
 
 class Character(object):
-    def __init__(self, name="Someone", pronouns=THEY):
+    def __init__(self, name="Someone", pron="they"):
         self.name = name
-        self.pronouns = pronouns
-        for k,v in PKEY.items():
-            self.__setattr__(k, pronouns[v])
-            self.__setattr__(k.title(), pronouns[v].title())
-            self.__setattr__(k.upper(), pronouns[v].upper())
+        self.pronouns = pronouns[pron.upper()]
+        for k,v in pronouns["PINDEX"].items():
+            self.__setattr__(k, self.pronouns[v])
+            self.__setattr__(k.title(), self.pronouns[v].title())
+            self.__setattr__(k.upper(), self.pronouns[v].upper())
 
     def __str__(self):
         return self.name
@@ -21,8 +22,8 @@ if __name__ == "__main__":
 
     people = {}
     people["finn"] = Character("Finn")
-    people["erik"] = Character("Erik", HE)
-    people["ada"] = Character("Ada", SHE)
+    people["erik"] = Character("Erik", "he")
+    people["ada"] = Character("Ada", "she")
 
     example = """{finn} is testing {finn.their} code.
 {erik} is helping! {erik.They} and {finn} use different pronouns.
