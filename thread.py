be765aa383dab5b1c0724f3c70a026b46c819408
Finn Ellis <relsqui@chiliahedron.com>
2016-08-02 17:59:09 -0700

    Comments for everybody!

--- thread.py
+++ thread.py
@@ -12,17 +12,22 @@ import random
 import json
 import sys
 
+
+# Grab the filename from the command line and open it.
 if len(sys.argv) == 2:
     chapterfile = sys.argv[1]
 else:
     print("Please pass exactly one argument: a chapter filename.")
     sys.exit(1)
-
 with open(chapterfile) as f:
     chapter = json.loads(f.read())
 
+
+# Reorder the scene list randomly.
 random.shuffle(chapter["scenes"])
 
+
+# Generate and print the story.
 if chapter["introduction"]:
     print(chapter["introduction"])
 
@@ -33,13 +38,17 @@ for story in chapter["story"]:
         try:
             scene = chapter["scenes"].pop()
             print(scene.format(**story))
+            # If the print worked, we had the right variables available. 
             found_scene = True
             break
         except KeyError:
+            # Didn't have the right variables.
+            # Set that scene aside and keep looking.
             rejects.append(scene)
+    # Put the rejected scenes for this story piece back.
     chapter["scenes"].extend(rejects)
     if not found_scene:
-        print("Couldn't find an approprate scene for story with these keys:", ", ".join(story.keys()))
+        print("Couldn't find an appropriate scene for story with these keys:", ", ".join(story.keys()))
         sys.exit(2)
 
 if chapter["conclusion"]:
