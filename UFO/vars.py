2ebb9603b000124329691d45a76ed082aa9950b4
Finn Ellis <relsqui@chiliahedron.com>
2016-08-11 01:24:59 -0700

    Add the first couple pieces of the UFO story.

--- UFO/vars.py
+++ UFO/vars.py
@@ -1,5 +1,10 @@
 {
-    "story": [],
+    "story": [
+        {
+            "background":"the ship landed just a few nights ago. Where by landed, I mean crashed. A few people saw a big burst of light and heard the noise and asked the local astronomer about it, but she told them it was probably a meteorite and that she'd go investigate in the morning. Probably some folks went to see for themselves, but if they did, they didn't find it, or didn't say anything about it",
+            "question":"Why would the astronomer tell people the spaceship was a meteorite?"
+        },
+    ],
     "globals": {
         "proto": 0,
         "sk": 1
